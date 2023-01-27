897d3985f1388f8a8fd97dfffc235cad01fa7b18
chilusoft <mukukachilu@gmail.com>
2023-01-27 18:04:43 +0200

    Demonstrated the spread operator

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ const [company, university, waterBody] = objects;
 console.log(
     `${company} is a computer company\n${university} is an Engineering institute in America\n${waterBody} is an ocean bordered by eastern coast of Africa and the Southern coast of Asia.`
 );
+
+// Spread Operator
+// merges values of objects/arrays
+var data_set_one = [1,2,3,4];
+var data_set_two = [7,8,9];
+var merged_data_set = [...data_set_one, ...data_set_two, 10, 11, 12]
+
+console.log(
+    `Dataset One ----> ${data_set_one}\nDataset Two ----> ${data_set_two}\nMerege Dataset --> ${merged_data_set}`
+);
